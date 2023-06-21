7551f0aa0c9bd19c516c0c9f2d0ec228cd427036
yuliyago <yuuliya02@proton.me>
2023-06-21 22:11:45 +0300

    Type casting work was done.

--- src/main/java/lang/print/gaps/task6/PrePostIncrement.java
+++ src/main/java/lang/print/gaps/task6/PrePostIncrement.java
@@ -6,9 +6,9 @@ public class PrePostIncrement {
         int b = 7;
         int c = 12;
         int d = 15;
-
+c++;
         // use post-increment operator in the mathematical expression  
-        int x =   a   + b +   c   + d;
+        int x =   a++  + b +   c   + d;
         System.out.println("Expected x is 40, x = " + x);
         // print the updated value of a, b, c, and d  
         System.out.println("The updated value of a = " + a + ", b = " + b + ", c = " + c + " and d = " + d);
